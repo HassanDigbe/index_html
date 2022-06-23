26b20cc53564514f08f257a6a57400b0d47705c0
Hassan Idris <105268328+HassanDigbe@users.noreply.github.com>
2022-06-23 01:23:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstname = prompt('enter your name');
+const height = prompt('enter you height');
+const country = prompt('enter your country');
+const check = prompt('enter to check if the statement are correct'); 
+const Info =document.write(`HI ${firstname},you are ${height} ft tall and your nationality is ${country}.`)
+            
